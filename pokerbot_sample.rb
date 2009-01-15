952176fe3dbdbde9e61bb87ec0ff9b806e2227b8
time <time.in.world@gmail.com>
2009-01-15 18:50:26 +0100

    More examples in the sample

--- pokerbot_sample.rb
+++ pokerbot_sample.rb
@@ -1,20 +1,34 @@
 class PokerBot
   def initialize
+    # Shortcut for OpenHoldem
     @oh = OpenHoldem
   end
   
   def process_state(state)
+    # The state is passed in a raw string containing the structure that must be decoded if needed
     0.0
   end
 
   def process_query(query)
     puts "Received query: #{query.inspect}"
-    if query == "dll$alli"
-      1.0
-    else
-      0.0
+
+    case query
+    when "dll$rais" # OpenHoldem found dll$rais in a formula and requests a value
+      # Get hole card ranks
+      r0 = @oh["$$pr0"]
+      r1 = @oh["$$pr1"]
+
+      if r0 == r1 and r0 >= 10 # If it's a pair above TT
+	return 1.0
+      end
     end
+    # Return 0 in other cases
+    0.0
   end
 end
 
-OpenHoldem.bot = PokerBot.new
+# Create a new bot instance
+bot = PokerBot.new
+
+# Tells OpenHoldem module to process requests on this instance
+OpenHoldem.bot = bot
