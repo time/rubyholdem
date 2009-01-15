cbe80f6d05efb7db1fd0c1feefd66e5b9169af17
time <time.in.world@gmail.com>
2009-01-15 12:26:51 +0100

    Added copyright notice

--- rubyholdem_spec.rb
+++ rubyholdem_spec.rb
@@ -1,3 +1,21 @@
+=begin
+  Copyright 2009 Time
+
+  This file is part of RubyHoldem.
+
+  RubyHoldem is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  RubyHoldem is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with RubyHoldem.  If not, see <http://www.gnu.org/licenses/>.
+=end
 
 require 'rubyholdem'
 
